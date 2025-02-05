3092758ff8b6e7d2fac36cd267511c9f045c4620
Vlad Smyrnov <vladsimovv@gmail.com>
2025-02-05 11:57:08 +0200

    Add convertor miles to km

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
 public class Main {
+    public static final double M = 1.609;
     public static void main(String[] args) {
+
         System.out.println("App for converting miles into kilometres");
+        double mile = 1;
+        System.out.println(mile + " mile is"+ milesToKilometres(mile)+ " kilometres");
+
+    }
+
+    public static double milesToKilometres(double miles){
+        return miles / M;
     }
 }
+
